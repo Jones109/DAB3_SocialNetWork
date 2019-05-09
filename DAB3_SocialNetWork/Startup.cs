52fac620d031a86d48d428ba6ef3ec02c89d7be5
Jonas <au544262@post.au.dk>
2019-05-09 11:00:21 +0200

    Created Service, Model and Controller for Post

--- DAB3_SocialNetWork/Startup.cs
+++ DAB3_SocialNetWork/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DAB3_SocialNetWork.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,7 @@ namespace DAB3_SocialNetWork
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<PostService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 

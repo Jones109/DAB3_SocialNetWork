48c383fb482b0f38da011eab303b38061ff34344
Tobias Mønster <au572284@uni.au.dk>
2019-05-10 10:01:25 +0200

    wall

--- SocialNetwork/Data/ApplicationDbContext.cs
+++ SocialNetwork/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Models;
 
 namespace SocialNetwork.Data
 {
@@ -12,5 +13,6 @@ namespace SocialNetwork.Data
             : base(options)
         {
         }
+        public DbSet<SocialNetwork.Models.Wall> Wall { get; set; }
     }
 }

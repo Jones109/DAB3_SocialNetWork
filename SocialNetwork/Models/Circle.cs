a6a21a6cd02a9b768e533541a7e1e022bfaa43ba
JonathanFL <j.lydemann@gmail.com>
2019-05-13 10:52:16 +0200

    Circle start

---
+++ SocialNetwork/Models/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SocialNetwork.Models
+{
+    public class Circle
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("WallId")]
+        public string WallId { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("OwnerId")]
+        public string OwnerId { get; set; }
+
+        [BsonElement("Admins")]
+        public List<User> Admins { get; set; }
+    }
+}
